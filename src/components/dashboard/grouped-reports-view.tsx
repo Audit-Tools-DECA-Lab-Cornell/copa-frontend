e4162934fdbceacaf34b34fba8cb785526c9b12e
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2026-05-11 12:13:22 -0400

    format code

--- src/components/dashboard/grouped-reports-view.tsx
+++ src/components/dashboard/grouped-reports-view.tsx
@@ -784,9 +784,9 @@ export function GroupedReportsView({
 													{header.isPlaceholder
 														? null
 														: flexRender(
-															header.column.columnDef.header,
-															header.getContext()
-														)}
+																header.column.columnDef.header,
+																header.getContext()
+															)}
 												</TableHead>
 											))}
 										</TableRow>
