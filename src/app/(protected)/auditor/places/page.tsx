a25032a42f20c2bee077ef1a5bd3229dacca7bdc
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2026-06-05 18:03:43 -0400

    Remove redundant auditor places redirect

--- src/app/(protected)/auditor/places/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-/**
- * Auditors do not have a standalone places page.
- * Assigned places are shown on the auditor dashboard.
- */
-export default function AuditorPlacesRedirect() {
-	redirect("/auditor/dashboard");
-}
